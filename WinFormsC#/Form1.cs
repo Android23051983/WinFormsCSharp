1600947d236f40469fdf761b82dd306671d0b3ad
Andrei <ikar2008@hotmail.com>
2023-03-18 22:51:42 +0300

    Добавьте файлы проекта.

---
+++ WinFormsC#/Form1.cs
@@ -0,0 +1,36 @@
+namespace WinFormsC_ {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            int i = 0;
+            int strlLength = 0;
+            String caption = "Резюме";
+            String message = "Здравствуйте. Меня зовут Щербаков Андрей Владимирович";
+            strlLength += message.Length;
+            i++;
+            DialogResult result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (result == DialogResult.OK) {
+                message = "В данный момент я студент Академии \"ТОР\"";
+                caption += " \"продолжение\"";
+                strlLength += message.Length;
+                i++;
+                result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK) {
+                    i++;
+                    message = "Скоро я стану первоклассным программистом";
+                    strlLength += message.Length;
+                    int temp = strlLength / i;
+                    result = MessageBox.Show(message, "Кол-во MB: " + i + ", букв: " + strlLength + ", знаков на MB: " + temp, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (result == DialogResult.OK) {
+                        Application.Exit();
+                    }
+                }
+            }
+        }
+    }
+
+
+}
